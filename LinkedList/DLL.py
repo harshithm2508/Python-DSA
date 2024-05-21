43f8a8be076570681b0bf7b4cc76dcefd8918c3c
harshithm2508 <harshithm2508@gmail.com>
2024-05-21 10:00:28 +0530

    addition at tail of DLL

--- LinkedList/DLL.py
+++ LinkedList/DLL.py
@@ -33,7 +33,7 @@ class DLL:
 
 
     
-    #   Inserting node at the beginning of the DLL
+    #   Inserting node at the beginning of the DLL          TC -> O(1) SC->O(1)
     def insertAtBegin(self,data):
         new_node = Node(data)
         if not self.head:
@@ -43,11 +43,33 @@ class DLL:
             self.head.prev = new_node
             self.head = new_node
 
+    
+    #   Inserting node at the end of DLL
+    def insertAtTail(self,data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while last.next is not None:
+            last = last.next
+        last.next = new_node
+        new_node.prev = last
+
+
+
+
+
+
 dll = DLL()
 dll.insertAtBegin(5)
 dll.insertAtBegin(4)
 dll.insertAtBegin(3)
 dll.insertAtBegin(2)
 dll.insertAtBegin(1)
+dll.insertAtTail(6)
+dll.insertAtTail(7)
+print(dll.head.data)
 dll.printList()
-print(dll.head.prev)
